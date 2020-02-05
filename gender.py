5a067aba1253ba433f6973a6911febde7aa16239
eduardo_decarvalho <eduardobcarvalho@outlook.com>
2020-02-04 22:47:48 -0300

    by class

--- gender.py
+++ gender.py
@@ -22,4 +22,29 @@ plt.subplot2grid((3, 4), (0, 3))
 df.Sex[df.Survived == 1].value_counts(normalize=True).plot(kind="bar", alpha=0.5, color=[female_color, "b"])
 plt.title("Gender of survived")
 
+plt.subplot2grid((3, 4), (1, 0), colspan=4)
+for x in [1, 2, 3]:
+    df.Survived[df.Pclass == x].plot(kind="kde")
+plt.title("Class wrt Survived")
+plt.legend(("1st", "2st", "3st"))
+
+plt.subplot2grid((3, 4), (2, 0))
+df.Survived[(df.Sex == "male") & (df.Pclass == 1)].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
+plt.title("Rich Men Survived")
+
+plt.subplot2grid((3, 4), (2, 1))
+df.Survived[(df.Sex == "male") & (df.Pclass == 3)].value_counts(normalize=True).plot(kind="bar", alpha=0.5)
+plt.title("Poor Men Survived")
+
+plt.subplot2grid((3, 4), (2, 2))
+df.Survived[(df.Sex == "female") & (df.Pclass == 1)].\
+    value_counts(normalize=True).plot(kind="bar", alpha=0.5, color=[female_color])
+plt.title("Rich Women Survived")
+
+plt.subplot2grid((3, 4), (2, 3))
+df.Survived[(df.Sex == "female") & (df.Pclass == 3)].\
+    value_counts(normalize=True).plot(kind="bar", alpha=0.5, color=[female_color])
+plt.title("Poor Women Survived")
+
+
 plt.show();
